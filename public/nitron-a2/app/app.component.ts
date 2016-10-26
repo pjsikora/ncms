bdfaedd7fbe53d085ffd700ec6021bd9d930071e
piotr <fedojo.com@gmail.com>
2016-10-26 17:54:49 +0200

    A2 preparation check

---
+++ public/nitron-a2/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import {PageListComponent} from './page/pagelist.component';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Nitron</h1>',
+
+
+})
+
+export class AppComponent {
+
+}
